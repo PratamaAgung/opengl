6099bd6dfb6ba4a3b169941efeef9acd565442f0
PratamaAgung <apratamamia@gmail.com>
2018-03-25 14:32:46 +0700

    get all path

--- src/convert_to_point.py
+++ src/convert_to_point.py
@@ -1,17 +1,32 @@
 from svgpathtools import svg2paths
-paths, attributes = svg2paths("image/car.svg")
+paths, attributes = svg2paths("image/car_2.svg")
 
 def normalize(x):
-    return (x - 250)/500
+    return (x - 550)/1100
 
 def printPoint(x, y):
     print(str(normalize(x)) + "f, " + str(-1* normalize(y)) + "f, 1.0f, 0.0f, 0.0f, 0.0f,") 
 
-path = paths[0]
-for element in path:
-    if (type(element).__name__ == "CubicBezier"):
-        printPoint(element.start.real, element.start.imag)
-        printPoint(element.end.real, element.end.imag)
-    elif (type(element).__name__ == "Line"):
-        printPoint(element.start.real, element.start.imag)
-        printPoint(element.end.real, element.end.imag)
+curr_x = 0.0
+curr_y = 0.0
+error = 4.0
+for path in paths:
+    for element in path:
+        if (type(element).__name__ == "CubicBezier"):
+            if (abs(curr_x - element.start.real) > error and abs(curr_y - element.start.imag) > error):
+                printPoint(element.start.real, element.start.imag)
+                curr_x = element.start.real
+                curr_y = element.start.imag
+            if (abs(curr_x - element.end.real) > error and abs(curr_y - element.end.imag) > error):
+                printPoint(element.end.real, element.end.imag)
+                curr_x = element.end.real
+                curr_y = element.end.imag
+        elif (type(element).__name__ == "Line"):
+            if (abs(curr_x - element.start.real) > error and abs(curr_y - element.start.imag) > error):
+                printPoint(element.start.real, element.start.imag)
+                curr_x = element.start.real
+                curr_y = element.start.imag
+            if (abs(curr_x - element.end.real) > error and abs(curr_y - element.end.imag) > error):
+                printPoint(element.end.real, element.end.imag)
+                curr_x = element.end.real
+                curr_y = element.end.imag
