9c1c90946348905fd021628bd02b4fb25999d8cd
Dery Rahman A <dery.ra@gmail.com>
2018-03-27 17:01:41 +0700

    Add tire

--- src/car/main.cpp
+++ src/car/main.cpp
@@ -50,6 +50,8 @@ int createShader(const std::string& vertexShader, const std::string& fragmentSha
   return program;
 }
 
+
+
 const char * loadShader(const char * file_path){
   std::string shader_code;
 	std::ifstream shader_stream(file_path, std::ios::in);
@@ -65,6 +67,43 @@ const char * loadShader(const char * file_path){
   return shader_code.c_str();
 }
 
+void createTire(float* vertices, float x, float y, float r, int side, float* tire_color){
+  float deg = 360/side;
+  for(int i=0; i<side*6; i+=6){
+    if(i == 0){
+      vertices[i] = x;
+      vertices[i+1] = y;
+      vertices[i+2] = 1.0f;
+    } else {
+      vertices[i] = x + (r * cos(i*deg*M_PI/180.0));
+      vertices[i+1] = y + (r * sin(i*deg*M_PI/180.0));
+      vertices[i+2] = 1.0f;
+    }
+    vertices[i+3] = tire_color[0];
+    vertices[i+4] = tire_color[1];
+    vertices[i+5] = tire_color[2];
+  }
+}
+
+void createVAOVBO(float* vertices, unsigned int size, unsigned int* vbo, unsigned int* vao) {
+    unsigned int vbo2;
+    glGenBuffers(1, &vbo2);
+    glBindBuffer(GL_ARRAY_BUFFER, vbo2);
+    glBufferData(GL_ARRAY_BUFFER, size, vertices, GL_STATIC_DRAW);
+
+    *vbo = vbo2;
+
+    unsigned int vao2;
+    glGenVertexArrays(1, &vao2);
+    glBindVertexArray(vao2);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3*sizeof(float)));
+    glEnableVertexAttribArray(1);
+
+    *vao = vao2;
+}
+
 int main(int argc, char** argv) {
     if (!glfwInit()) {
         fprintf(stderr, "failed to initialize glfw\n");
@@ -78,7 +117,7 @@ int main(int argc, char** argv) {
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
     GLFWwindow* window;
-    window = glfwCreateWindow( 800, 600, "Car", NULL, NULL);
+    window = glfwCreateWindow( 1000, 1000, "Car", NULL, NULL);
     if(!window){
         fprintf( stderr, "Failed to open GLFW window.\n" );
         glfwTerminate();
@@ -93,7 +132,7 @@ int main(int argc, char** argv) {
         return -1;
     }
 
-    float vertices[] = {
+    float car_vertices[] = {
       // back
       0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f,
       0.8677449999999999f, 0.05218999999999994f, 1.0f, 0.0f, 0.0f, 0.0f,
@@ -115,25 +154,27 @@ int main(int argc, char** argv) {
       0.8677449999999999f, 0.05218999999999994f, 1.0f, 0.0f, 0.0f, 0.0f,
     };
 
+    int side = 360;
+    float tire_front_vertices[side*6];
+    float tire_back_vertices[side*6];
+    float tire_color[3];
+    tire_color[0] = 1.0f;
+    tire_color[1] = 0.5f;
+    tire_color[2] = 0.0f;
+    createTire(tire_front_vertices, -0.4523350000000001f, -0.22941000000000004f, 0.17f, side, tire_color);
+    createTire(tire_back_vertices, 0.6348599999999999f, -0.22941000000000004f, 0.17f, side, tire_color);
+
     const char* vertex_shader_source_code = loadShader("./src/car/CarVS.vs");
     const char* fragment_shader_source_code = loadShader("./src/car/CarFS.fs");
 
     unsigned int shader_program = createShader(vertex_shader_source_code, fragment_shader_source_code);
 
-    unsigned int vbo;
-    glGenBuffers(1, &vbo);
-    unsigned int vao;
-    glGenVertexArrays(1, &vao);
-
-    glBindVertexArray(vao);
-    glBindBuffer(GL_ARRAY_BUFFER, vbo);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
-    glEnableVertexAttribArray(0);
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3*sizeof(float)));
-    glEnableVertexAttribArray(1);
+    unsigned int vbo, vbo2, vbo3;
+    unsigned int vao, vao2, vao3;
+    createVAOVBO(car_vertices, sizeof(car_vertices),&vbo,&vao);
+    createVAOVBO(tire_front_vertices, sizeof(tire_front_vertices),&vbo2,&vao2);
+    createVAOVBO(tire_back_vertices, sizeof(tire_back_vertices),&vbo3,&vao3);
     glUseProgram(shader_program);
-    glBindVertexArray(vao);
 
     while(!glfwWindowShouldClose(window)) {
         processInput(window);
@@ -141,7 +182,12 @@ int main(int argc, char** argv) {
         glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
-        glDrawArrays(GL_TRIANGLE_FAN, 0, 28);
+        glBindVertexArray(vao);
+        glDrawArrays(GL_TRIANGLE_FAN, 0, 19);
+        glBindVertexArray(vao2);
+        glDrawArrays(GL_TRIANGLE_FAN, 0, 360);
+        glBindVertexArray(vao3);
+        glDrawArrays(GL_TRIANGLE_FAN, 0, 360);
 
         glfwSwapBuffers(window);
         glfwPollEvents();
