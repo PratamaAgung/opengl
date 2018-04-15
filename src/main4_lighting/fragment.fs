ebb63b8bb35f49f3089094ac319af1746f6f7ccf
irfanariq <13515112@std.stei.itb.ac.id>
2018-04-15 20:08:52 +0000

    change tire texture

--- src/main4_lighting/fragment.fs
+++ src/main4_lighting/fragment.fs
@@ -11,6 +11,6 @@ uniform sampler2D texture2;
 void main()
 {
 	// linearly interpolate between both textures (80% container, 20% awesomeface)
-	//FragColor = vec4(ourColor, 1.0f);
+	// FragColor = vec4(ourColor, 1.0f);
 	FragColor = mix(texture(texture1, TexCoord), texture(texture2, TexCoord), 0.2);
 }
